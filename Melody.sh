d862264e78960d5d2982d239a481589003966ab9
Jerome <153257261+good-girls@users.noreply.github.com>
2024-07-24 12:40:38 +0800

    Add files via upload

--- Melody.sh
+++ Melody.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-
 sh_v="1.0.1"
-
 huang='\033[33m'
 bai='\033[0m'
 lv='\033[0;32m'
@@ -9,27 +7,22 @@ lan='\033[0;34m'
 hong='\033[31m'
 kjlan='\033[96m'
 hui='\e[37m'
-
 # 检查是否以root用户运行
 if [[ $EUID -ne 0 ]]; then
   echo "${hong}请使用root用户运行脚本！${bai}"
   exit 1
 fi
-
 echo "${lan}Melody，你的幸运之声！——Docker for Telegram Lottery Bot${bai}"
 echo "${lan}欢迎使用Melody抽奖机器人！${bai}"
 read -p "${lan}是否继续运行脚本？(y/n) ${bai}" answer
-
 if [[ "$answer" =~ ^[yY]$ ]]; then
   echo "${lv}继续运行脚本...${bai}"
-
   while true; do
     echo "${lan}Melody Lottery-Bot Script：${bai}"
     echo "${lan}1. 直接安装${bai}"
     echo "${lan}2. docker 安装${bai}"
     echo "${lan}0. 退出脚本${bai}"
     read -p "${lan}请输入你的选择 [0-2]: ${bai}" choice
-
     if [[ "$choice" =~ ^[0-2]$ ]]; then
       if [[ "$choice" == "0" ]]; then
         echo "${lan}退出脚本...${bai}"
@@ -52,11 +45,9 @@ if [[ "$answer" =~ ^[yY]$ ]]; then
         pip3 install "python-telegram-bot[job-queue]"
         curl -L -o /root/Melody-sh.py https://raw.githubusercontent.com/good-girls/Melody/main/Melody-sh.py
         echo "${lv}获取脚本成功！${bai}"
-
         # 获取 API 并替换脚本
         read -p "${lan}请输入你从@BotFather获取的完整机器人API： ${bai}" API
         sed -i "s/application = Application.builder().token(\"你的机器人token\").build()/application = Application.builder().token(\"$API\").build()/g" /root/Melody-sh.py
-
         # 执行脚本
         echo "${lv}正在启动 Melody-sh.py 脚本...${bai}"
         python3 /root/Melody-sh.py &
@@ -71,14 +62,12 @@ if [[ "$answer" =~ ^[yY]$ ]]; then
           echo "${lv}正在安装 docker...${bai}"
           curl -sSL https://get.docker.com/ | sh
         fi
-
         # 获取 API 并替换命令
         read -p "${lan}请输入你从@BotFather获取的完整机器人API： ${bai}" API
         docker run -d \
           -e TELEGRAM_BOT_TOKEN="$API" \
           --name melody \
           josemespitia/melody:latest
-
         echo "${lv}Melody-Bot 已启动，后台运行...${bai}"
       fi
       break
@@ -89,7 +78,6 @@ if [[ "$answer" =~ ^[yY]$ ]]; then
       echo ""
     fi
   done
-
 elif [[ "$answer" =~ ^[nN]$ ]]; then
   echo "${lan}退出脚本...${bai}"
   exit 1
